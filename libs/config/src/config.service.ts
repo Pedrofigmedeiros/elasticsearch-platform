97af28d22baed5dd1131b88bff04da08902f5b06
Pedro Medeiros <pedrofigmedeiros@gmail.com>
2026-07-11 17:38:08 +0200

    chore: update the configs

--- libs/config/src/config.service.ts
+++ libs/config/src/config.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import env from 'env-var';
 
 @Injectable()
-export class ConfigService {}
+export class ConfigService {
+  get elasticUrl(): string {
+    return env
+      .get('ELASTICSEARCH_NODE')
+      .required()
+      .asString();
+  }
+
+  get apiPort(): number {
+    return env
+      .get('API_PORT')
+      .default('8080')
+      .asPortNumber();
+  }
+}

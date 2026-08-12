34f2df4cf658bf171ad2132a37d098afb78b513b
Pedro Medeiros <pedrofigmedeiros@gmail.com>
2026-08-12 15:06:10 +0200

    feat: create ui version 1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { JobsSearchHttpModule } from './app/jobs-search/jobs-search.module';
 import { ConfigService } from 'libs/config';
+import { join } from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(JobsSearchHttpModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    JobsSearchHttpModule,
+  );
   const config = app.get(ConfigService);
+
+  app.useStaticAssets(join(process.cwd(), 'apps/api/public'));
+
   await app.listen(config.apiPort);
 }
 void bootstrap();
